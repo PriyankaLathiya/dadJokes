8c9079a8f02c53d8f720c5c94eeb62b07c7f84fd
Priyanka <priyanka@Priyankas-Air.domain.name>
2023-03-12 21:39:33 +0530

    uploading the project files

---
+++ DadJokedWeb/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DadJokedWeb.Models;
+using System.Text;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace DadJokedWeb.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        return View();
+    }
+
+    public async Task<IActionResult> GetJokes(int? count = 1)
+    {
+        try
+        {
+            string apiResponse = "";
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://localhost:7176/api/DadJokes/randomjoke/" + count))
+                {
+                    apiResponse = await response.Content.ReadAsStringAsync();
+                }
+            }
+            return Content(apiResponse);            
+        }
+        catch (Exception e)
+        {
+            return Content("{\"success\":false}", "application/json");
+        }
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
+    
---
+++ DadJokesWebApi/Controllers/DadJokesController.cs
@@ -0,0 +1,77 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DadJokesWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DadJokesController : ControllerBase
+    {
+        private HttpClient _client;
+        private const string APIKEY = "X-RapidAPI-Key", APIValue = "a4790af60cmshc5bd233302d5975p10e423jsn671070f7b75f";
+
+        public DadJokesController(HttpClient client)
+        {
+            _client = client;
+        }
+            
+        [HttpGet]
+        [Route("randomjoke/{count:int?}")]
+        public async Task<string> RandomJoke(int? count = 1)
+        {
+            string APIURL = $"https://dad-jokes.p.rapidapi.com/random/joke?count={count}";
+            HttpRequestMessage request = new HttpRequestMessage();
+            request.RequestUri = new Uri(APIURL);
+            request.Method = HttpMethod.Get;
+            request.Headers.Add(APIKEY, APIValue);
+
+            HttpResponseMessage response = await _client.SendAsync(request);
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                if (string.IsNullOrEmpty(result))
+                    return "Success";
+                else
+                    return result;
+            }
+            else if (response.StatusCode == HttpStatusCode.Unauthorized)
+            {
+                throw new UnauthorizedAccessException();
+            }
+            else
+            {
+                throw new Exception(await response.Content.ReadAsStringAsync());
+            }
+        }
+
+        [HttpGet]
+        [Route("jokecount")]
+        public async Task<string> JokeCount()
+            {
+            string APIURL = $"https://dad-jokes.p.rapidapi.com/joke/count";
+            HttpRequestMessage request = new HttpRequestMessage();
+            request.RequestUri = new Uri(APIURL);
+            request.Method = HttpMethod.Get;
+            request.Headers.Add(APIKEY, APIValue);
+            
+            HttpResponseMessage response = await _client.SendAsync(request);
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                if (string.IsNullOrEmpty(result))
+                    return "Success";
+                else
+                    return result;
+            }
+            else if (response.StatusCode == HttpStatusCode.Unauthorized)
+            {
+                throw new UnauthorizedAccessException();
+            }
+            else
+            {
+                throw new Exception(await response.Content.ReadAsStringAsync());
+            }
+        }
+    }
+}
+
